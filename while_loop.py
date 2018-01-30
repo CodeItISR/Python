53fcd0595cd1e80e3a37cabf87bd65aeac5b6487
CodeItISR <35896574+CodeItISR@users.noreply.github.com>
2018-01-30 15:20:20 +0200

    Create while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,18 @@
+'''
+An example for a program that inputs python 3 times
+
+count = 0
+var = 'Python'
+
+while(count < 3):
+    print(var)
+    count = count + 1
+
+'''
+
+list = ['python' ,'c' ,'php' ,'asm']
+lenght = len(list)
+i=0
+while(i < lenght):
+    print(list[i])
+    i =  i + 1
