173c8e3aa95408717d8dcf7cde715914c93cc95d
CodeItISR <35896574+CodeItISR@users.noreply.github.com>
2018-02-02 16:37:36 +0200

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,26 @@
+# Print each chat in string
+
+string = 'Python'
+
+for s in string:
+  print(s)
+  
+#===============
+# Print each string in the list and its letters
+
+list = ['Python' ,'c' , 'asm']
+
+for s in list:
+  print(s)
+  for l in s:
+    print(l)
+  print("============")
+
+#===============
+
+# Create a list with the numbers 0 - 9 [0,1,2,3,4,5,6,7,8,9]
+
+list = []
+
+for i in range(0,10):
+  list.append(i)
