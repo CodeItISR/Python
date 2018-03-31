3d13aa3a3345c66b0c41c5142d8cf0392ec8c7b1
CodeItISR <35896574+CodeItISR@users.noreply.github.com>
2018-03-31 20:44:20 +0300

    Add files via upload

---
+++ Frameworks/Checks/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name="Checks", 
+      version="0.1", 
+      url="https://github.com/CodeItISR/Python/tree/master/Frameworks/Checks",
+      maintainer='Checks',
+      py_modules=['inspect','sys','traceback'],
+      classifiers=[
+      'Development Status :: 1 - Planning',
+      'Environment :: Console',
+      'Natural Language :: English',
+      'Operating System :: OS Independent',
+      'license=MIT LICENSE'
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6'],
+      description="Easy framework to add condition to a function's arguments") 
